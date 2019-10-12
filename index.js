4a5f4a09fb778dffc724b73e63dac4be3f368edd
Logan Ellis <mrloganellis@gmail.com>
2019-10-12 08:57:51 -0400

    just need to learn how to get the value from the submit button

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 'use strict';
 
+let select = document.getElementById('breedSelect')
+let number = $('.inputs').val()
+
+
 function getImage() {
-    fetch('https://dog.ceo/api/breeds/image/random')
+    console.log(number)
+    fetch(`https://dog.ceo/api/breeds/image/random/"${number}"`)
         .then(response => response.json()) 
         .then(responseJson => 
             displayResults(responseJson))
         .catch(error => alert('Please check your internet or the server might be down.'));
-        console.log('getImage ran')
+        console.log('getImage ran the if statement')
     }
 
 function displayResults(responeJson) {
     $('.img-results').replaceWith(`<img src="${responeJson.message}" alt="Random Doggo!" class="img-results">`)
     $('.results').removeClass('hidden');
-    console.log('displayResults ran');
 }
 
 function pressSubmit() {
     $('.submit').on('click', function(s) {
         s.preventDefault();
         getImage();
-        console.log('pressSubmit ran');
     });
 }
 
@@ -27,3 +30,4 @@ $(function() {
     console.log('App loaded! Waiting for you to hit submit!');
     pressSubmit();
 });
+
