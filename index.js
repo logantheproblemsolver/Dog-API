775bee9e6b23c4afbb76341db43041529a6a2e3a
Logan Ellis <mrloganellis@gmail.com>
2019-10-13 12:15:19 -0400

    I just need to add the ability to see multiple images

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
 
-let select = document.getElementById('breedSelect')
 
-
-function getImage(number) {
-    fetch(`https://dog.ceo/api/breeds/image/random/"${number}"`)
+function getImage(number, select) {
+    if (select===""){
+        fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+            .then(response => response.json()) 
+            .then(responseJson => 
+                displayResults(responseJson))
+            .catch(error => alert('Please check your internet or the server might be down.'));
+    } else {
+    fetch(`https://dog.ceo/api/breed/${select}/images/random/${number}`)
         .then(response => response.json()) 
         .then(responseJson => 
             displayResults(responseJson))
         .catch(error => alert('Please check your internet or the server might be down.'));
-        console.log('getImage ran the if statement')
     }
+}
 
 function displayResults(responeJson) {
     $('.img-results').replaceWith(`<img src="${responeJson.message}" alt="Random Doggo!" class="img-results">`)
@@ -19,10 +24,12 @@ function displayResults(responeJson) {
 
 function pressSubmit() {
     $('.submit').on('click', function(s) {
-        let number = $('.inputs').val()
-        console.log(number);
+        let breed = $('.breeds').val().replace('-', '/');
+        let select = breed;
+        let number = $('.inputs').val();
+        console.log(select);
         s.preventDefault(); 
-        getImage(number);
+        getImage(number, select);
     });
 }
 
